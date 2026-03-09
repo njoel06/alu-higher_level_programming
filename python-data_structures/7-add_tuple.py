d5afd3a9f5298936d635aaf354301c7fd5eb000e
njoel06 <njoel06@gmail.com>
2026-03-09 04:42:11 -0700

    Task 7 Complete

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 7-add_tuple.py
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Ensure tuples have at least 2 elements, pad with 0 if needed
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0 
+
+
+    return (a1 + b1, a2 + b2)
